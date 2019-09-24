7fae3a8796f5548346c347be1838f8d816edd6f6
lipingping <875784820@qq.com>
2019-09-24 10:52:08 +0800

    Signed-off-by: lipingping <875784820@qq.com> 优化路由加载方式

--- src/router/async.js
+++ src/router/async.js
@@ -1,18 +1,20 @@
 import loading from '../components/common/loading'
 import failed from '../components/common/failed'
 //异步加载组件优化函数
-export  default (c) => ({
-  // 需要加载的组件 (应该是一个 `Promise` 对象)
-  component: c,
-  // 异步组件加载时使用的组件
-  loading: loading,
-  // 加载失败时使用的组件
-  error:failed,
-  // 展示加载时组件的延时时间。默认值是 200 (毫秒)
-  delay: 300,
-  // 如果提供了超时时间且组件加载也超时了，
-  // 则使用加载失败时使用的组件。默认值是：`Infinity`
-  timeout: 1000
-})
+export  default (c) =>{
+  return ()=>({
+    // 需要加载的组件 (应该是一个 `Promise` 对象)
+    component: c,
+    // 异步组件加载时使用的组件
+    loading: loading,
+    // 加载失败时使用的组件
+    error:failed,
+    // 展示加载时组件的延时时间。默认值是 200 (毫秒)
+    delay: 300,
+    // 如果提供了超时时间且组件加载也超时了，
+    // 则使用加载失败时使用的组件。默认值是：`Infinity`
+    timeout: 1000
+  })
+}
 
 
--- src/router/routerLink.js
+++ src/router/routerLink.js
@@ -1,9 +1,9 @@
 import lazyComponent from './async.js'
 
 export default {
-  Home: () => lazyComponent(import('@/pages/Home')),
-  Mine: () => lazyComponent(import('@/pages/mine/Mine')),
-  MusicHouse: () => lazyComponent(import('@/pages/musicHouse/index')),
-  Find: () => lazyComponent(import('@/pages/find/index')),
-  LocalMusic: () => lazyComponent(import('@/pages/localMusicList')),
+  Home: lazyComponent(import('@/pages/Home')),
+  Mine: lazyComponent(import('@/pages/mine/Mine')),
+  MusicHouse: lazyComponent(import('@/pages/musicHouse/index')),
+  Find: lazyComponent(import('@/pages/find/index')),
+  LocalMusic: lazyComponent(import('@/pages/localMusicList')),
 }
